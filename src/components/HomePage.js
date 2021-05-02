30013a45d53bf980f29abde781fba10d7b299bd8
Tony Shifflett <tonyshifflett@tonys-mbp.lan>
2021-05-02 00:28:22 -0500

    created components folder, HomePage.js, ZipSearch.js with dummy code

---
+++ src/components/HomePage.js
@@ -0,0 +1,12 @@
+import ZipSearch from './ZipSearch'
+
+const HomePage = ()=>{
+    return (
+        <>
+            <h1>HomePage Component</h1>
+            <ZipSearch/>
+        </>
+    )
+}
+
+export default HomePage
---
+++ src/components/ZipSearch.js
@@ -0,0 +1,9 @@
+
+
+const ZipSearch = ()=>{
+    return (
+        <h2>ZipSearch component</h2>
+    )
+}
+
+export default ZipSearch
